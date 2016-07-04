0293800646daadedb9c07059edcc7c94094e7753
George Skrapits <gskrap@gmail.com>
2016-07-04 18:45:27 -0400

    fix buttons on homepage

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   window.onpopstate = function() {
     $('.btn-huge').css("background-color", "#B6DEFF");
+    $('.btn-huge-link').css("background-color", "#B6DEFF");
+    $('.link-text').css("background-color", "#B6DEFF");
   }
   $('.btn-huge').on('mouseover', function(){
     $(this).css("background-color", "black");
