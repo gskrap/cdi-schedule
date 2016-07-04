fcfafbf7bfe3ef8a5e7e37dc1767c5a3e83baec0
George Skrapits <gskrap@gmail.com>
2016-07-03 22:19:49 -0400

    teacher hours

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -4,4 +4,8 @@ class Teacher < ActiveRecord::Base
   def full_name
     self.first_name + " " + self.last_name
   end
+
+  def hours
+    ((self.dance_classes.inject(0){ |sum,x| sum + x.duration }) / 60).to_f
+  end
 end
