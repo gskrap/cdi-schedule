1d6f87d19d6bfc479bbf484acd68a44dea54a5ad
George Skrapits <gskrap@gmail.com>
2016-07-09 15:44:00 -0400

    add buttons on class index to view classes only by group

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function(){
+  $('.class-sort-btn').on('click', function(){
+    $('.class-column').hide();
+    $('#'+$(this).attr('id').replace('-btn','')+'-column').show()
+  })
+  $('#show-all-btn').on('click', function(){
+    $('.class-column').show();
+  })
+})
