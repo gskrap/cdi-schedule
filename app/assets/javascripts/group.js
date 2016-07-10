ecaaae517ed7c610660e692e35025114b0dbe01f
George Skrapits <gskrap@gmail.com>
2016-07-10 14:17:35 -0400

    set up mailto

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -2,6 +2,14 @@
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
   $('.attendance-btn').on('click', function(){
-    $(this).children().toggleClass('btn-default btn-success')
+    $(this).children().toggleClass('btn-default btn-success');
+    $(this).parent().toggleClass('not-attending');
   })
+  $('.email-btn').on('click', function (event) {
+    event.preventDefault();
+    var email = "cdiauditions@gmail.com";
+    var subject = document.getElementById('class_name_field').value + " - Attendance - " + $($('.varsity')[0]).text();
+    var emailBody = "The following students are absent:\n";
+    window.location = 'mailto:' + email + '?subject=' + subject + '&body=' +   emailBody;
+  });
 })
